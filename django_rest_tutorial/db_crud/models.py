71e3549bad95284f601a6ba1007359262b89082c
eclairsameal <skolleclair@bric.jp>
2023-09-29 15:03:09 +0900

    Write models corresponding to the database and generate migrations files

--- django_rest_tutorial/db_crud/models.py
+++ django_rest_tutorial/db_crud/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
+
+from django.db import models
+
+
+class Singer(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    debut_year = models.DateField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    singer = models.ForeignKey(Singer, on_delete=models.CASCADE)
+    release_date = models.DateField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Music(models.Model):
+    id = models.AutoField(primary_key=True)
+    song = models.CharField(max_length=255)
+    singer = models.ForeignKey(Singer, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    album_no = models.SmallIntegerField()
+    song_length = models.IntegerField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.song
