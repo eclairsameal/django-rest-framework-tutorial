e5a2c8ff78b5e94e09db5ad6164b98b059117dad
eclairsameal <skolleclair@bric.jp>
2023-09-29 14:58:30 +0900

    Modify the database settings in settings.py

--- django_rest_tutorial/django_rest_tutorial/settings.py
+++ django_rest_tutorial/django_rest_tutorial/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'django_rest_tutorial.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
 
