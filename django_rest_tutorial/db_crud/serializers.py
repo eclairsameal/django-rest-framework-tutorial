cef2ada0ea27ddfa47d9c1a5c2f95e515cdd8884
eclairsameal <skolleclair@bric.jp>
2023-10-06 11:08:53 +0900

    Add serialized file

--- django_rest_tutorial/db_crud/serializers.py
+++ django_rest_tutorial/db_crud/serializers.py
@@ -5,16 +5,19 @@ from .models import Singer, Album, Music
 class SingerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Singer
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ('name', 'debut_year')
 
 
 class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ('name', 'singer', 'release_date')
 
 
 class MusicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ('id', 'song', 'singer', 'album', 'album_no', 'song_length')
